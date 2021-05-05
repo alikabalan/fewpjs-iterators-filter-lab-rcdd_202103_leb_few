faaf61f6c8a7880e04d96472a22888c59cf08974
Ali Kabalan <ali.kabalan.1994@gmail.com>
2021-05-05 23:32:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 // Code your solution here
 
-// const findMatching = (array, string) => {
-//   return array.filter(x => x.toLowerCase() === string.toLowerCase())
-// }
-//  const fuzzyMatch = (array, string) => {
-//   return array.filter(x => x.slice(0, string.length) === string)
-// }
-//  const matchName = (array, string) => {
-//   return array.filter(x => x.name === string)
-// }
-
-
 const findMatching = (array, string) => {
-  return array.fikter(x => x.toLowerCase() === string.toLowerCase())
+  return array.filter(x => x.toLowerCase() === string.toLowerCase())
+}
+ const fuzzyMatch = (array, string) => {
+  return array.filter(x => x.slice(0, string.length) === string)
+}
+ const matchName = (array, string) => {
+  return array.filter(x => x.name === string)
 }
 
-const fuzzyMatch = (array, string) => {
-  return array.filter(x => x.ChatAt[0] === string.ChatAt[0])
-}
+
